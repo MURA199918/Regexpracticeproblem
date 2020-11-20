8b0643e598069b4c944f7f4d746ee700d5f646ad
MURA199918 <muralisaranr18@gmail.com>
2020-11-20 11:39:49 +0530

    check user last name

--- src/regexpracticeproblem/regex.java
+++ src/regexpracticeproblem/regex.java
@@ -5,12 +5,20 @@ public class regex {
     public static void main(String args[]) {
     	System.out.println("Enter user name");
     	Scanner sc=new Scanner(System.in);
-    	String name=sc.nextLine();
-    	if(name.matches("^[A-Z][A-Z a-z]{3,25}$")) {
+    	String firstname=sc.nextLine();
+    	if(firstname.matches("^[A-Z][A-Z a-z]{3,25}$")) {
     		System.out.println("Valid first name");
     	}
     	else {
     		System.out.println("Invalid first name");
+    	}
+    	
+    	String lastname=sc.nextLine();
+    	if(lastname.matches("^[A-Z][A-Z a-z]{3,25}$")) {
+    		System.out.println("Valid last name");
+    	}
+    	else {
+    		System.out.println("Invalid last name");
     	}
     }
 
